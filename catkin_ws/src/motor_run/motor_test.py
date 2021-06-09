6cf8162e4089b76f8924310aac544e8100c2d1a6
nlinthacumn <nlinthacum@zagmail.gonzaga.edu>
2021-06-09 16:22:26 -0700

    sub prints for or back

--- catkin_ws/src/motor_run/motor_test.py
+++ catkin_ws/src/motor_run/motor_test.py
@@ -2,48 +2,57 @@
 import rospy
 from time import sleep
 import RPi.GPIO as GPIO
-from std_msgs.msg import Char
+from std_msgs.msg import String
 
 def subscriber():
-	sub = rospy.Subscriber('keyboard_publish', Char, callback_function)
-	
-	rospy.spin()
+    sub = rospy.Subscriber('keyboard_publish', String, callback_function)
+    
+    rospy.spin()
 
 def callback_function(message):
-	rospy.loginfo("I received: %c"%message.data)
+    rospy.loginfo(message.data)
+    command = message.data
+    if command == 'w':
+        rospy.loginfo("forward")
+    elif command == 's':
+        rospy.loginfo("backward")
 
 if __name__ == "__main__":
-	rospy.init_node("keyboard_subscriber")
-	subscriber()
+    rospy.init_node("keyboard_subscriber")
+    subscriber()
 
-# DIR = 20
-# STEP = 21
-# CW = 1
-# CCW = 0
-# SPR = 200
-# 
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(DIR, GPIO.OUT)
-# GPIO.setup(STEP, GPIO.OUT)
-# GPIO.output(DIR, CW)
-# 
-# step_count = SPR
-# delay = 0.005
-# 
-# # for x in range(step_count):
-# #     GPIO.output(STEP, GPIO.HIGH)
-# #     sleep(delay)
-# #     GPIO.output(STEP, GPIO.LOW)
-# #     sleep(delay)
-#     
-# sleep(.5)
-# GPIO.output(DIR,CCW)
-# 
-# for x in range(step_count):
-#     GPIO.output(STEP, GPIO.HIGH)
-#     sleep(delay)
-#     GPIO.output(STEP, GPIO.LOW)
-#     sleep(delay)
-#     
-# # GPIO.cleanup()
-# 
+DIR = 20
+STEP = 21
+CW = 1
+CCW = 0
+SPR = 200
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+GPIO.output(DIR, CW)
+
+step_count = SPR
+delay = 0.005
+
+
+
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+    
+sleep(.5)
+GPIO.output(DIR,CCW)
+
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+    
+GPIO.cleanup()
+
+
+
