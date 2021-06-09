ff698f4435427d3e109913d7eb15ad528c849be9
nlinthacumn <nlinthacum@zagmail.gonzaga.edu>
2021-06-09 12:07:03 -0700

    publishes letter when pressed

--- catkin_ws/src/motor_run/motor_test.py
+++ catkin_ws/src/motor_run/motor_test.py
@@ -1,36 +1,49 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
+import rospy
 from time import sleep
 import RPi.GPIO as GPIO
+from std_msgs.msg import Char
 
-DIR = 20
-STEP = 21
-CW = 1
-CCW = 0
-SPR = 200
+def subscriber():
+	sub = rospy.Subscriber('keyboard_publish', Char, callback_function)
+	
+	rospy.spin()
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(STEP, GPIO.OUT)
-GPIO.output(DIR, CW)
+def callback_function(message):
+	rospy.loginfo("I received: %s"%message.data)
 
-step_count = SPR
-delay = 0.005
+if __name__ == "__main__":
+	rospy.init_node("keyboard_subscriber")
+	subscriber()
 
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
-    
-sleep(.5)
-GPIO.output(DIR,CCW)
-
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
-    
-GPIO.cleanup()
-
+# DIR = 20
+# STEP = 21
+# CW = 1
+# CCW = 0
+# SPR = 200
+# 
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(DIR, GPIO.OUT)
+# GPIO.setup(STEP, GPIO.OUT)
+# GPIO.output(DIR, CW)
+# 
+# step_count = SPR
+# delay = 0.005
+# 
+# # for x in range(step_count):
+# #     GPIO.output(STEP, GPIO.HIGH)
+# #     sleep(delay)
+# #     GPIO.output(STEP, GPIO.LOW)
+# #     sleep(delay)
+#     
+# sleep(.5)
+# GPIO.output(DIR,CCW)
+# 
+# for x in range(step_count):
+#     GPIO.output(STEP, GPIO.HIGH)
+#     sleep(delay)
+#     GPIO.output(STEP, GPIO.LOW)
+#     sleep(delay)
+#     
+# # GPIO.cleanup()
+# 
--- catkin_ws/src/motor_run/publisher_node.py
+++ catkin_ws/src/motor_run/publisher_node.py
@@ -7,26 +7,31 @@ from std_msgs.msg import Char
 
 
 def publisher():
-	pub	= rospy.Publisher('keyboard_publish', Char, queue_size=10)
+    pub	= rospy.Publisher('keyboard_publish', Char, queue_size=10)
 
-	rate	= rospy.Rate(1)
+    rate	= rospy.Rate(1)
 
-	msg_to_publish = Char()
-	
-	command = getch.getch()
+    msg_to_publish = Char()
+    
+    command = getch.getch()
 
-	while not rospy.is_shutdown():
-		char_to_publish = "Publishing %c"%command
+    while not rospy.is_shutdown():
+        command = getch.getch()
+        
+        char_to_publish = "Publishing %c"%command
 
-		msg_to_publish.data = char_to_publish
-		pub.publish(msg_to_publish)
+        msg_to_publish.data = char_to_publish
+        pub.publish(msg_to_publish)
 
-		rospy.loginfo(char_to_publish)
+        rospy.loginfo(char_to_publish)
 
-		rate.sleep()
+        rate.sleep()
 
 
 if __name__== "__main__":
-	rospy.init_node("simple_publisher")
-	publisher()
+    rospy.init_node("simple_publisher")
+    publisher()
+
+
+
 
