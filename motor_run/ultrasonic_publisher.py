e5b5fa72fbcd08ce1dec135dabf706a99d9080d4
nlinthacum <nlinthacum@zagmail.gonzaga.edu>
2021-06-24 13:51:08 -0700

    added another ultransonic sensor

--- motor_run/ultrasonic_publisher.py
+++ motor_run/ultrasonic_publisher.py
@@ -4,6 +4,7 @@ import rospy
 import getch
 import RPi.GPIO as GPIO
 import time
+from time import sleep
 
 from std_msgs.msg import Char
 from std_msgs.msg import String
@@ -12,30 +13,63 @@ from std_msgs.msg import Float32
 GPIO.setmode(GPIO.BCM)
  
 #set GPIO Pins
-GPIO_TRIGGER = 19
-GPIO_ECHO = 26
+GPIO_TRIGGER1 = 19
+GPIO_ECHO1 = 26
+
+GPIO_TRIGGER2 = 20
+GPIO_ECHO2 = 21
  
 #set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+GPIO.setup(GPIO_TRIGGER1, GPIO.OUT)
+GPIO.setup(GPIO_ECHO1, GPIO.IN)
 
-def distance():
+GPIO.setup(GPIO_TRIGGER2, GPIO.OUT)
+GPIO.setup(GPIO_ECHO2, GPIO.IN)
+
+def distance1():
     # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
+    GPIO.output(GPIO_TRIGGER1, True)
  
     # set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
+    GPIO.output(GPIO_TRIGGER1, False)
  
     StartTime = time.time()
     StopTime = time.time()
  
     # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
+    while GPIO.input(GPIO_ECHO1) == 0:
         StartTime = time.time()
  
     # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
+    while GPIO.input(GPIO_ECHO1) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+
+def distance2():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER2, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER2, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO2) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO2) == 1:
         StopTime = time.time()
  
     # time difference between start and arrival
@@ -50,6 +84,7 @@ def distance():
 
 def publisher():
     pub	= rospy.Publisher('ultrasonic_publish1', Float32, queue_size=1)
+    pub2	= rospy.Publisher('ultrasonic_publish2', Float32, queue_size=1)
 
     rate	= rospy.Rate(50) #should be 1 hz
 
@@ -58,21 +93,28 @@ def publisher():
     
 
     while not rospy.is_shutdown():
-        distance = distance()
+        sens_distance1 = distance1()
+        sens_distance2 = distance2()
     
         
-        char_to_publish = "Publishing %c"%distance
+       # char_to_publish = sens_distance1
+       # char_to_publish = sens_distance2
 
       #  msg_to_publish.data = char_to_publish
-        pub.publish(distance)
+        pub.publish(sens_distance1)
+        pub2.publish(sens_distance2)
 
-        rospy.loginfo(distance)
+        rospy.loginfo(sens_distance1)
+        rospy.loginfo(sens_distance2)
+        
+        sleep(.1)
 
         rate.sleep()
 
 
 if __name__== "__main__":
-    rospy.init_node("ultrasonic_publish1")
+    #rospy.init_node("ultrasonic_publish1") #idk if this is right
+    rospy.init_node("ultrasonic_publish")
     publisher()
 
 
